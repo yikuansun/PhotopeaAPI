c38707fda09d9aeac907e6027f5653faa7a698d5
Yikuan Sun <yikuansun2015@gmail.com>
2021-04-23 15:29:39 -0400

    Update photopea.js

--- photopea.js
+++ photopea.js
@@ -1,13 +1,13 @@
 var Photopea = {
-    initEmbed: async function(elem_to_append_to, environment) {
+    initEmbed: async function(elem_to_append_to, config) {
         // Example usage: Photopea.initEmbed(document.body);
-        // environment is optional.
+        // config is optional.
         // This will not work on Chrome if third-party cookies and site data are blocked.
         var iframe = document.createElement("iframe");
         iframe.style.border = "0";
         iframe.style.width = "100%";
         iframe.style.height = "100%";
-        if (environment) iframe.src = "https://www.photopea.com#" + encodeURI(environment);
+        if (config) iframe.src = "https://www.photopea.com#" + encodeURI(config);
         else iframe.src = "https://www.photopea.com";
         elem_to_append_to.appendChild(iframe);
         var myPromise = new Promise(function(resolve, reject) {
