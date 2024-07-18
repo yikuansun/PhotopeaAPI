812bff4e1c6b1cb685ff81619c20e3e17be77fd2
Yikuan Sun <yikuansun2015@gmail.com>
2024-07-17 20:08:33 -0400

    Function for creating photopea embed

--- src/index.js
+++ src/index.js
@@ -2,4 +2,33 @@ export default class Photopea {
     static sayHello() {
         return 'Hi, I am Tom.'
     }
+
+    static async initEmbed(parentElement, config) {
+        let _config = "";
+        if (typeof(config) == "object") _config = JSON.stringify(config);
+        else if (typeof(config) == "string") _config = config;
+        let frame = document.createElement("iframe");
+        frame.style.border = "0";
+        frame.style.width = "100%";
+        frame.style.height = "100%";
+        if (config) frame.src = "https://www.photopea.com#" + encodeURI(_config);
+        else frame.src = "https://www.photopea.com";
+        parentElement.appendChild(frame);
+        let waitForInit = new Promise(function(res, rej) {
+            let messageHandle = function(e) {
+                if (e.data == "done") {
+                    let pea = new Photopea(frame.contentWindow);
+                    window.removeEventListener("message", messageHandle);
+                    res(pea);
+                }
+            };
+            window.addEventListener("message", messageHandle);
+        });
+        return await waitForInit;
+    }
+
+    contentWindow;
+    constructor(contentWindow) {
+        this.contentWindow = contentWindow;
+    }
 }
